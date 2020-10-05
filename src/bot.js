febdc21a38de42faec1514dc63ee3f6ca50d5441
Mushi2216 <thomahaine@gmail.com>
2020-10-05 16:05:16 +0200

    hello, kick and ban command for discord bot using nodeJS

---
+++ src/bot.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+
+const client = new Client();
+const PREFIX = '$';
+
+client.on('ready',()=>{
+    console.log(`${client.user.username} has logged in`)
+});
+
+client.on('message', (message) => {
+    if (message.author.bot) return;
+    if (message.content === 'hello' || "Hello") {
+        message.channel.send('Bip Boup Hello!');
+    }
+});
+
+ client.on('message', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const { CMD_NAME, ...args } = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
+
+        if (CMD_NAME === 'kick') {
+            if(message.member.hasPermission('KICK_MEMBERS')) return message.reply('You do not have permissions to use that command');
+            if(args.length === 0) return message.reply('Please provide an ID');
+            const member = message.guild.members.cache.get(args[0]);
+            if(member) {
+                member.kick()
+                .then((member) => 
+                    message.channel.send(`${member} was kicked`))
+                    .catch((err) => message.channel.send('I cannot kick that user :('));
+            } else {
+                message.channel.send('That member was not found');
+            }
+        } else if (CMD_NAME === 'ban') {        
+                if(message.member.hasPermission('BAN_MEMBERS'))
+                    return message.reply('You do not have permissions to use that command');
+                if(args.length === 0) return message.reply('Please provide an ID');
+
+            try {
+                const user = await message.guild.members.ban(args[0]);
+                message.channel.send('User was banned successfully')
+            } catch (err) {
+                message.channel.send('An error occured, Either I do not have the permissions or the user was not found');
+            }
+        }
+    }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
